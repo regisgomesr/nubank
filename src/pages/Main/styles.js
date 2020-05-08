b72786f298e6c0e9462e43815f4e8ac424ec164f
Regis Ribeiro <regisgomesr@gmail.com>
2020-05-08 14:26:35 -0300

    Add Card in Main

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -6,3 +6,29 @@ export const Container = styled.View`
   background: #8B10AE;
   padding-top: ${getStatusBarHeight()}px;
 `;
+
+export const Content = styled.View`
+  flex: 1;
+  max-height: 400px;
+  z-index: 5;
+`;
+
+export const Card = styled.View`
+  flex: 1;
+  background: #FFF;
+  border-radius: 4px;
+  margin: 0 20px;
+  height: 100%;
+`;
+
+export const CardHeader = styled.View``;
+
+export const CardContent = styled.View``;
+
+export const Title = styled.Text``;
+
+export const Description = styled.Text``;
+
+export const CardFooter = styled.View``;
+
+export const Annotation = styled.Text``;
