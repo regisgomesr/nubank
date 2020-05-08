acf37e0a6945d9ea68ca00efeaadf35386f50e93
Regis Ribeiro <regisgomesr@gmail.com>
2020-05-08 14:37:06 -0300

    Stylized Card

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -21,14 +21,37 @@ export const Card = styled.View`
   height: 100%;
 `;
 
-export const CardHeader = styled.View``;
+export const CardHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+`;
 
-export const CardContent = styled.View``;
+export const CardContent = styled.View`
+  flex: 1;
+  padding: 0 30px;
+  justify-content: center;
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-size: 13px;
+  color: #999;
+`;
 
-export const Description = styled.Text``;
+export const Description = styled.Text`
+  font-size: 32px;
+  margin-top: 3px;
+  color: #333;
+`;
 
-export const CardFooter = styled.View``;
+export const CardFooter = styled.View`
+  padding: 30px;
+  background: #EEE;
+  border-radius: 4px;
+`;
 
-export const Annotation = styled.Text``;
+export const Annotation = styled.Text`
+  font-size: 12px;
+  color: #333;
+`;
