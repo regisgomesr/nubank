4aef13b32b21dd6d3cd8d71467583e8eb50c2c08
Regis Ribeiro <regisgomesr@gmail.com>
2020-05-14 15:23:46 -0300

    Main - onHandlerStateChange arranging animation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -11,6 +11,7 @@ import Menu from '~/components/Menu';
 import { Container, Content, Card, CardHeader, CardContent, Title, Description, CardFooter, Annotation } from './styles';
 
 export default function Main() {
+  let offset = 0;
   const translateY = new Animated.Value(0);
 
   const animatedEvent = Animated.event(
@@ -25,7 +26,31 @@ export default function Main() {
   )
 
   function onHandlerStateChange(event) {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      let opened = false;
+      const { translateY } = event.nativeEvent;
 
+      offset += translateY;
+
+      if (translateY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 200,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+
+    }
   }
 
   return(
